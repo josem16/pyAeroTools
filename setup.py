306789d720f18642285972ee1c551d89c3e2b18e
José Manuel Márquez <josem16@users.noreply.github.com>
2024-03-12 23:53:00 -0700

    Create setup.py
    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pyAeroTools',
+    version='0.0.1',
+    author='Jose Manuel Marquez Jr.',
+    packages=['pyAeroTools',],
+    license='LICENSE.txt',
+    long_description=open('README.md').read(),
+)
